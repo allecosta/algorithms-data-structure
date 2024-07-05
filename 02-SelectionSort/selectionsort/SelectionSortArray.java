3b5d05693ba709afbb27dcf459704ae3b191940c
allecosta <alle_costa10@outlook.com>
2024-07-05 11:56:44 -0300

    fix: Selection sort implementation

---
+++ 02-SelectionSort/selectionsort/SelectionSortArray.java
@@ -0,0 +1,17 @@
+package selectionsort;
+
+public class SelectionSortArray {
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
+
--- 02-SelectionSort/selectionsort/SelectionSortList.java
+++ 02-SelectionSort/selectionsort/SelectionSortList.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SelectionSort {
+public class SelectionSortList {
 
-    // Uma primeira opção para ordenar um array de números
-    public static List<Integer> selectionSortOne(List<Integer> array) {
+    public static List<Integer> selectionSort(List<Integer> array) {
         List<Integer> newArray = new ArrayList<>(array.size());
 
         int size = array.size();
@@ -15,7 +14,8 @@ public class SelectionSort {
         for (int i = 0; i < size; i++) {
             int smallest = findSmallest(array);
 
-            newArray.remove(smallest);
+            newArray.add(array.get(smallest));
+            array.remove(smallest);
         }
 
         return newArray;
@@ -27,25 +27,12 @@ public class SelectionSort {
         int smallestIndex = 0;
 
         for (int i = 0; i < array.size(); i++) {
-            if (array.get(1) < smallest) {
+            if (array.get(i) < smallest) {
                 smallest = array.get(i);
                 smallestIndex = i;
             }
         }
 
         return smallestIndex;
-    }
-
-    // Uma segunda opção para ordenar um array de números
-    public static void selectionSortTwo(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[i]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
     }
 }
